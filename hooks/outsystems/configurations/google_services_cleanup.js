5ba3a4f2ce87a99c48ecfe1d631cbf37653f8d9f
lazloman <lorenzo@thethurmans.com>
2021-04-30 12:30:27 -0500

    Update google_services_cleanup.js

--- hooks/outsystems/configurations/google_services_cleanup.js
+++ hooks/outsystems/configurations/google_services_cleanup.js
@@ -1,12 +1,19 @@
 var path = require("path");
+var fs = require("fs");
 var utils = require("./utils");
+
 module.exports = function(context) {
     return new Promise(function(resolve, reject) {
         var wwwpath = utils.getWwwPath(context);
-        var configPath = path.join(wwwpath, ".firebase");
-        console.log("Cleaning up ", configPath);
-        // clean up google-services folder from source directory in project
-        utils.rmNonEmptyDir(configPath);
+        fs.readdir(wwwpath, function (err, files) {
+           files.forEach(file => {
+                var configPath = path.join(wwwpath, ".firebase");
+                console.log("Cleaning up ", configPath);
+                // clean up google-services folder from source directory in project
+                utils.rmNonEmptyDir(configPath);
+            })
+        }
+        
         return resolve();
     });
 };
