daa77e022992895bc34234861d986a3a320ae7ec
lazloman <lorenzo@thethurmans.com>
2021-05-19 10:18:32 -0500

    Update google_services_cleanup.js

--- hooks/outsystems/configurations/google_services_cleanup.js
+++ hooks/outsystems/configurations/google_services_cleanup.js
@@ -5,18 +5,27 @@ var utils = require("./utils");
 module.exports = function(context) {
     return new Promise(function(resolve, reject) {
         var wwwpath = utils.getWwwPath(context);
-        fs.readdir(wwwpath, function(err, files) {
+       fs.readdir(wwwpath, function(err, files) {
 
 	files.forEach(function(file) {
-		if (file.isDirectory() && file.indexOf(".firebase") != -1) {
+		var stats = fs.statSync(file);
+		var isDir = stats.isDirectory(file);
+		
+		if (file.indexOf(".firebase") !== -1 && isDir) {
+			console.log(isDir);
+			console.log(file.indexOf(".firebase"));
 			var configPath = path.join(wwwpath, file);
-			console.log("Cleaning up ", configPath);
-			// clean up google-services folder from source directory in project
-			utils.rmNonEmptyDir(configPath);
+			
+			try{
+				fs.rmdir(configPath, { recursive: true }, (err) => {
+				console.log("Removed ", configPath);
+			});
+			} catch(err) {
+				console.log("Error deleting directory " + err);
+			}
 		}
 		});
-	}
-   
+	})   
         
         return resolve();
     });
